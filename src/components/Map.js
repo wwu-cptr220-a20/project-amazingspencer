8988ce8ed67148c6e22bb1b0e18492535f3e6a1d
Samuel S. Hernandez <samuel.hernandez@wallawalla.edu>
2020-11-11 20:41:53 -0600

    added googleMaps Wrapper and API

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-export default class Map extends Component{
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
 
-    render(){
-
-        
-    }
+export class Map extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={3}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: 0,
+            lng: 0
+          }
+        }
+      />
+    );
+  }
 }
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBbPk_NONuQ7r1buVhCf_R4c32j_E660xc'
+})(MapContainer);
