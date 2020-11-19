3fce26d6445886070aa3a42d20a6c7247470a9be
Samuel S. Hernandez <samuel.hernandez@wallawalla.edu>
2020-11-19 00:00:57 -0600

    disabled default UI and set map type to 'hybid' to remove grey bars

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,21 +4,32 @@ import GoogleMapReact from 'google-map-react';
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default class Map extends Component {
+
+
     static defaultProps = {
         center: {
             lat: 0,
             lng: 0
         },
-        zoom: 3
+        zoom: 3,
+        disableDefaultUI: true,
+        mapTypeId: "hybrid"
+
     };
 
     render() {
+
+
         return (
+
+            
             <div style={{ height: '100vh', width: '100%' }}>
                 <GoogleMapReact
                     bootstrapURLKeys={{ key: "AIzaSyBbPk_NONuQ7r1buVhCf_R4c32j_E660xc" }}
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
+                    defaultMaptype={this.props.mapTypeId}
+                    defaultUI={this.props.disableDefaultUI}
                 >
                     <AnyReactComponent
                         lat={59.955413}
