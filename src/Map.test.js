1915929eddf852c47d612b25e0376f076bc38bb8
System Administrator <root@Haydens-MacBook-Pro-2.local>
2020-11-19 20:21:38 -0800

    Adding Map Test

---
+++ src/Map.test.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { act } from 'react-dom/test-utils';
+import { render } from 'react-dom';
+let container = null;
+beforeEach(() => {
+// setup a DOM element as a render target
+container = document.createElement("div");
+document.body.appendChild(container);
+});
+afterEach(() => {
+// cleanup on exiting
+unmountComponentAtNode(container);
+container.remove();
+container = null;
+});
+describe("Content and HTML is valid.", () => {
+    test("Map renders", () => {
+      jest.spynOn(global, "fetch").mockImplementation(()=>
+      Promise.resolve({
+          json: ()=>Promise.resolve(data)
+      })
+      );
+      await act(async() => {
+          render(<Map/>,container);
+      });
+      global.fetch.mockRestore();
+    })
+ 
+   })
