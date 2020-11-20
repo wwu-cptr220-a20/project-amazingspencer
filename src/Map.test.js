c59010512cc09b95be620b0bef94171a58cbbb15
amazingspencer <46183769+amazingspencer@users.noreply.github.com>
2020-11-19 20:56:38 -0800

    Edited Map.test.js

--- src/Map.test.js
+++ src/Map.test.js
@@ -2,29 +2,36 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { act } from 'react-dom/test-utils';
 import { render } from 'react-dom';
+
 let container = null;
 beforeEach(() => {
-// setup a DOM element as a render target
-container = document.createElement("div");
-document.body.appendChild(container);
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
 });
 afterEach(() => {
-// cleanup on exiting
-unmountComponentAtNode(container);
-container.remove();
-container = null;
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
 });
+
 describe("Content and HTML is valid.", () => {
-    test("Map renders", () => {
-      jest.spynOn(global, "fetch").mockImplementation(()=>
-      Promise.resolve({
-          json: ()=>Promise.resolve(data)
-      })
-      );
-      await act(async() => {
-          render(<Map/>,container);
-      });
-      global.fetch.mockRestore();
+
+    test("Map renders", async () => {
+        // Add some data here, like const data = {};
+
+        jest.spynOn(global, "fetch").mockImplementation(() =>
+            Promise.resolve({
+                json: () => Promise.resolve(data)
+            })
+        );
+        await act(async () => {
+            render(<Map />, container);
+        });
+
+        // Write your test here, like expect( this should be ).toBe( this )
+        global.fetch.mockRestore();
     })
- 
-   })
+
+})
