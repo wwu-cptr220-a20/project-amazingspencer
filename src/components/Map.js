0d0d420cdb6dd02eb6c66635149d99fa00cca0d5
Samuel S. Hernandez <samuel.hernandez@wallawalla.edu>
2020-11-21 17:12:57 -0600

    added marker and pulse to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import marker from '../img/marker.png';
 // Use of google-map-react from https://www.youtube.com/watch?v=OunoJaa-dnU&t=433s and https://www.npmjs.com/package/google-map-react
+
+
 
 export default class Map extends Component {
     constructor(props) {
@@ -41,7 +44,13 @@ export default class Map extends Component {
         this._isMounted = false;
     }
 
+
     render() {
+
+        const handleApiLoaded = (map, maps) => {
+            // use map and maps objects
+        };
+
         let { error, isLoading } = this.state;
         if (error) {
             return <div><h2>There was a problem with your request.</h2></div>;
@@ -51,37 +60,55 @@ export default class Map extends Component {
         }
 
         let countries = this.state.countries.map((data) => {
+
             return (
                 <div
+                    className="pin bounce"
+
                     key={data.country}
                     lat={data.countryInfo.lat}
                     lng={data.countryInfo.long}
                     style={{
-                        color: "black",
-                        backgroundColor: "#FFF",
-                        height: "25px",
-                        width: "35px",
+                        color: "blue",
                         textAlign: "center",
+                        fontFamily: "Roboto"
                     }}
+
                 >
-                    <img height="10px" src={data.countryInfo.flag} alt={data.country} />
+
+
+                    <img src={marker} alt={data.country} />
                     <br />
-                    {data.cases}
+
+                    <div className="pulse"/>
                 </div>
             );
         })
+        // data.countryInfo.flag
+        // {data.cases}
+        //
+
         return (
             <div style={{ height: '100vh', width: '100%' }}>
                 <GoogleMapReact
                     bootstrapURLKeys={{ key: "AIzaSyBbPk_NONuQ7r1buVhCf_R4c32j_E660xc" }}
-                    defaultCenter={{lat: 0, lng: 0}}
-                    defaultZoom={0}
-                    defaultMaptype={{mapTypeId: "hybrid"}}
-                    defaultUI={{disableDefaultUI: true}}
+                    defaultCenter={{ lat: 0, lng: 0 }}
+                    defaultZoom={3}
+                    yesIWantToUseGoogleMapApiInternals={true}
+                    onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+                    screenControl={{ fullscreenControl: false }}
+                    //hover Distance
+                    //On Bounds Change
+                    //On Child Click
+                    //On Child Mouse Enter
+                    //On Child MouseLeave
+
                 >
+
+
                     {countries}
                 </GoogleMapReact>
-            </div>
+            </div >
         );
     }
 }
